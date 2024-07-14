b74bd079f60046d02f9385e5c655abd35835414f
philpotti <89038510+philpotti@users.noreply.github.com>
2024-07-14 17:08:04 -0300

    pytestWorkshop lecture 03

--- test_file1.py
+++ test_file1.py
@@ -4,8 +4,12 @@ import pytest
 def func(x):
     return x + 1
 
+
+@pytest.mark.this
 def test_answer():
     assert func(3) == 4
 
+
+@pytest.mark.hello
 def test_answer2():
-    assert(6) == 7
+    assert (6) == 7
